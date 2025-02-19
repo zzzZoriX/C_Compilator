13b0bf3e4a3fdc201ad58720bc8773e6f4f269fe
vstlvl <zorix228337@gmail.com>
2025-02-19 16:04:15 +0300

    полностью написан скрипт главного файла

--- Code/Main.c
+++ Code/Main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include "c:/c_compilator/libraries/include/Interpritator.h"
+#include "c:/c_compilator/libraries/include/Compilator.h"
+
+extern ErrorStruct* result;
 
 #define DEBUG 0
 
 /*
     compile command
-    gcc ./code/main.c ./libraries/interpritator.c ./libraries/tokens.c ./libraries/strings.c ./libraries/objects.c ./libraries/errorhandler.c -o output_file
+    gcc ./code/main.c ./libraries/types.c ./libraries/calculator.c ./libraries/stack.c ./libraries/compilator.c ./libraries/tokens.c ./libraries/strings.c ./libraries/objects.c ./libraries/errorhandler.c -o output_file
 */
 
 int
@@ -20,23 +22,58 @@ main(int argc, char** argv){
     fprintf(stdout, "Program start\n");
     #endif
 
-    HeadObject* head_of_objects = Init_head_obj();
-
     FILE* input_file_stream = fopen(argv[1], "r");
     if(!input_file_stream){
         fprintf(stderr, "Error occured while opening input file");
         exit(1);
     }
 
-    Interpritator(input_file_stream, head_of_objects);
+    result = CreateError("NullErr", -2, -9);
 
-    fclose(input_file_stream);
+    #if DEBUG == 1
+    fprintf(stdout, "Error tracker thread create...\n");
+    #endif
+    pthread_t error_thread;
+    pthread_create(
+        &error_thread,
+        NULL,
+        CheckToError,
+        NULL
+    );
 
-    Release_head_obj(head_of_objects);
+    char* buffer;
+    length_n count_of_words;
+
+    #if DEBUG == 1
+    fprintf(stdout, "Get count of words...\n");
+    #endif
+
+    result = Read_line_before_symbol_from_file(&buffer, input_file_stream, EOF);
+    result = Count_of_words_in_line(&count_of_words, buffer, SEPARATOR, true);
+
+    fseek(input_file_stream, 0, SEEK_SET);
+
+    Token* tokens;
+
+    #if DEBUG == 1
+    fprintf(stdout, "Lexer...\n");
+    #endif
+    
+    result = Lexer(&tokens, input_file_stream);
+
+    #if DEBUG == 1
+    fprintf(stdout, "Parser...\n");
+    #endif
+    
+    result = Parser(tokens, count_of_words);
+
+    free(tokens);
 
     #if DEBUG == 1
     fprintf(stdout, "Program end\n");
     #endif
+
+    fclose(input_file_stream);
 
     return 0;
 }
